b78c6a9bd441f26e7fe40ac8b32c6d65bc8188b2
adam nunez <adam.a.nunez@gmail.com>
2022-07-09 03:49:56 -0500

    Adding luxon for datetimes

--- webpack/index.js
+++ webpack/index.js
@@ -3,5 +3,6 @@ import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import { DateTime } from "luxon";
 
 console.log("Hello world!")
