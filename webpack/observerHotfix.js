2254f6895e4a9f53b37837513f16cd92858eca75
adam nunez <adam.a.nunez@gmail.com>
2022-07-11 00:08:31 -0500

    Fixing observer over firing

--- webpack/observerHotfix.js
+++ webpack/observerHotfix.js
@@ -1,11 +1,12 @@
-
 let calendarObs = new MutationObserver((mutations) => {
-    let brokenIcon = document.querySelector("span.fc-icon-fa-gear")
-    if (brokenIcon) {
+    let broken = document.querySelector("span.fc-icon-fa-gear")
+    if (!broken) return
+    if (!broken.parentNode.querySelector(".fa-gear")) {
         let el = document.createElement("i")
         el.classList = "fa-solid fa-gear"
-        brokenIcon.parentNode.replaceChild(el, brokenIcon)
+        broken.parentNode.replaceChild(el, broken)
     }
+    broken.remove()
 })
 
 calendarObs.observe(document.getElementById("calendar"), {
