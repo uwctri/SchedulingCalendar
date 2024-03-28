dfec4e3bc6e80b148de0a1d4f2619dfb6e0becc4
adam nunez <adam.a.nunez@gmail.com>
2024-03-28 00:06:38 -0500

    Swap away from $ to jquery for easy search

--- webpack/config.js
+++ webpack/config.js
@@ -1,14 +1,17 @@
+// This file must use jQuery as the 'show.bs.modal' event
+// isn't available vai the vanilla JS listener 
+
 import "./style.less"
 
-$(document).ready(() => {
+jQuery(document).ready(() => {
 
     const prefix = "scheduling_calendar"
-    let $modal = $('#external-modules-configure-modal')
+    let $modal = jQuery('#external-modules-configure-modal')
 
     $modal.on("show.bs.modal", (event) => {
 
         // Making sure we are overriding this modules"s modal only.
-        if ($(event.target).data("module") !== prefix) return
+        if (jQuery(event.target).data("module") !== prefix) return
         $modal.addClass('calConfig')
 
         if (typeof ExternalModules.Settings.prototype.resetConfigInstancesOld === "undefined")
