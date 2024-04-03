819bc65eb62aa2fdcf0c92689628cce0c9cf8620
adam nunez <adam.a.nunez@gmail.com>
2024-04-03 03:16:58 -0500

    remove api endpoint

--- api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-    $result = $module->process(true);
-    RestUtility::sendResponse(200, $result, 'json');
-} catch (Exception $ex) {
-    RestUtility::sendResponse(400, $ex->getMessage());
-}
