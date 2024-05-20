5b84fad58ba76dc36fdbb370f141d7d88afc8884
adam nunez <adam.a.nunez@gmail.com>
2024-05-19 19:38:08 -0700

    Publish build improvs

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,13 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = merge(common, {
     mode: 'production',
-});
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
+    },
+})
