2fca2193fb75acafc403fecde6ab845ba9b62641
adam nunez <adam.a.nunez@gmail.com>
2024-05-31 23:14:53 -0500

    doc updates

--- docs.php
+++ docs.php
@@ -39,25 +39,25 @@ include APP_PATH_VIEWS . 'HomeTabs.php';
                     with some limitations.
                 </div>
             </div>
-            <div class="card my-4 card-primary">
+            <div id="basics" class="card my-4 card-primary">
                 <div class="card-header text-white fw-bold bg-primary bg-gradient">Calendar Basics</div>
                 <div class="card-body">
                     Go over every tab, scheduling, availability, my. And how to use.
                 </div>
             </div>
-            <div class="card my-4 card-primary">
+            <div id="config" class="card my-4 card-primary">
                 <div class="card-header text-white fw-bold bg-primary bg-gradient">Configuration</div>
                 <div class="card-body">
                     Go over all configuration options
                 </div>
             </div>
-            <div class="card my-4 card-primary">
+            <div id="admin" class="card my-4 card-primary">
                 <div class="card-header text-white fw-bold bg-primary bg-gradient">Admin Tools</div>
                 <div class="card-body">
                     Go over every tab, scheduling, availability, my. And how to use.
                 </div>
             </div>
-            <div class="card my-4 card-primary">
+            <div id="locs" class="card my-4 card-primary">
                 <div class="card-header text-white fw-bold bg-primary bg-gradient">Location Settings Structure</div>
                 <div class="card-body">
                     Go over every tab, scheduling, availability, my. And how to use.
@@ -92,20 +92,63 @@ include APP_PATH_VIEWS . 'HomeTabs.php';
                     </pre>
                 </div>
             </div>
-            <div class="card my-4 card-primary">
+            <div id="det" class="card my-4 card-primary">
                 <div class="card-header text-white fw-bold bg-primary bg-gradient">DET Integration</div>
                 <div class="card-body">
-                    Go over every tab, scheduling, availability, my. And how to use.
+                    If this feature is enabled in the configuration, the module will send a message to the DET endpoint,
+                    if one is set for the project, when anything (appointment or availability) is added, removed, or updated on the calendar.
+                    The message sent will be a POST request with a JSON body formatted as below. The endpoint should respond with a 200 status
+                    code message immediately upon receipt of the message and carry out any additional processing asynchronously.
+                    <br><br>
+                    Note: The internal id is a unique identifier for the appointment or availability. It is not the same as the record_id or any other identifer in REDCap.
+                    Currently it is only useful if you are able to directly query the database for more information. This limits the usefulness of the delete and update messages.
+                    <pre>
+                        <code>
+                            {
+                                redcap_url: Root URL of the REDCap installation,
+                                project_url: URL to the assoicated projecting ending in "/index.php?pid=[project-id]",
+                                project_id: The project id of the current project,
+                                username: The username of the current user,
+                                resource: Enum describing the resource that was impacted (Availability, Appointment),
+                                crud: Enum describing the operation that occured (create, update, delete. Read is never sent),
+                                ___
+                                // Only sent when resource is Appointment and crud is create
+                                start: Start datetime in ISO format,
+                                end: End datetime in ISO format,
+                                providers: Array of size one with provider username,
+                                locations: Array of size one with location name,
+                                subjects: Array of size one with subject record_id,
+                                visits: Array of size one with visit code,
+                                notes: Any notes that were added,
+                                ___
+                                // Only sent when resource is Availability and crud is create
+                                start: Start datetime in ISO format,
+                                end: End datetime in ISO format,
+                                providers: Array of size one with provider username,
+                                locations: Array of size one with location name,
+                                group: Availability code (also called group code),
+                                ___
+                                // Only sent when crud is update
+                                id: Internal id of the appointment or availability,
+                                providers: Array of size one with the new provider username (or previous if not changed),
+                                locations: Array of size one with the new location name (or previous if not changed),
+                                ___
+                                // Only sent when crud is delete
+                                id: Internal id of the appointment or availability
+                            }
+                        </code>
+                    </pre>
                 </div>
             </div>
-            <div class="card my-4 card-primary">
+            <div id="query" class="card my-4 card-primary">
                 <div class="card-header text-white fw-bold bg-primary bg-gradient">Query Parameters</div>
                 <div class="card-body">
-                    When linking to the calendar from an an instrument or an external source, you can use a few query parameters to configure the calendar.
+                    When building a form in REDCap you might want to add a button to send the user to the calendar; probably to schedule an appointment as a part of the workflow.
+                    When linking to the calendar from an an instrument, or any external source, you can appened a few extra query parameters to configure things.
 
                     <table class="table my-3">
                         <tr>
-                            <td><b>record</b> or <b>id</b></td>
+                            <td class="nowrap"><b>record</b> or <b>id</b></td>
                             <td>Preselect a subject. Useful if you are adding a link in a form to instruct the user to schedule the current subject.</td>
                         </tr>
                         <tr>
@@ -118,6 +161,8 @@ include APP_PATH_VIEWS . 'HomeTabs.php';
                         </tr>
                     </table>
 
+                    Custom buttons are usually added into descriptive fields and, as a result, normal REDCap field piping and smart variables can be used to build the URL.
+                    <br>
                     A typical link might look like this:
                     <pre>
                         <code>
@@ -125,7 +170,7 @@ include APP_PATH_VIEWS . 'HomeTabs.php';
                         </code>
                     </pre>
 
-                    A URL using both date and record might look like the below.
+                    A URL using both date and record might look like the below. Notice we have piped the date value from a REDCap field.
                     <pre>
                         <code>
                             [redcap-version-url]ExternalModules/?prefix=scheduling_calendar&page=index&pid=[project-id]&record=[record-name]&date=[some_date_field]
@@ -135,9 +180,11 @@ include APP_PATH_VIEWS . 'HomeTabs.php';
                     A URL using refer can use either "true" or an encoded URL. To do the latter requires some javascript to encode the URL. Consider using the Shazam EM for this.
                     <pre>
                         <code>
-                            let refer = encodeURIComponent(location.href)
+                            // Send the user to google after they are done with the calendar
+                            let refer = encodeURIComponent("https://www.google.com")
                             // Set this URL on a button or link. redcap_version is a global variable set by REDCap. 
-                            let url = location.href.split(redcap_version)[0] + `${redcap_version}/ExternalModules/?prefix=scheduling_calendar&page=index&pid=${pid}&refer=${refer}`
+                            let url = `${redcap_version}/ExternalModules/?prefix=scheduling_calendar&page=index&pid=${pid}&refer=${refer}`
+                            url = location.href.split(redcap_version)[0] + url
                         </code>
                     </pre>
                 </div>
@@ -153,7 +200,7 @@ include APP_PATH_VIEWS . 'HomeTabs.php';
 <script>
     // Format code blocks
     $("#pagecontainer pre").each((_, el) => {
-        let t = $(el).text().split('\n').filter((line) => line.trim().length > 0)
+        let t = $(el).text().split('\n').filter((line) => line.trim().length > 0).map((line) => line.replace("___", ""))
         let l = t[0].search(/\S/)
         $(el).text(t.map((line) => line.slice(l)).join('\n'))
     })
@@ -177,11 +224,17 @@ $HtmlPage->PrintFooter();
     }
 
     .card-header {
-        font-size: 1.35em;
+        font-size: 1.25em;
     }
 
-    .card-body {
-        font-size: 1.2em;
+    .card {
+        scroll-margin-top: 4em;
+    }
+
+    .card-body,
+    #toc a {
+        font-size: 1.1em;
+        color: black;
     }
 
     pre {
